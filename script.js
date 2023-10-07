97c1af46c972dded0d18fbcde3eeca903f695f24
cycankahuna <supreetvats55@gmail.com>
2023-10-07 19:37:42 +0530

    display new rgb colour every time page is loaded

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function generateRandomNmber(min, max) {
+const colorCodeContainer = document.getElementById("color-code");
+const optionContainer = document.getElementById("option-conatiner");
+let randomColor = null;
+
+function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function generateRandomColorRGM() {
-  const red = generateRandomNmber(0, 225);
-  const green = generateRandomNmber(0, 225);
-  const blue = generateRandomNmber(0, 225);
+function generateRandomColorRGB() {
+  const red = generateRandomNumber(0, 255);
+  const green = generateRandomNumber(0, 255);
+  const blue = generateRandomNumber(0, 255);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
+
+function startGame() {
+  let randomColor = generateRandomColorRGB();
+  colorCodeContainer.innerText = randomColor;
+
+  for (let i = 0; (i = 6); i++) {
+    const div = document.createElement("div");
+    div.style.background = generateRandomColorRGB();
+    optionContainer.append(div);
+  }
+}
+
+window.addEventListener("load", startGame);
